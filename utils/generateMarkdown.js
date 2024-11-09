93120b260d9589faa3b7a7f2d87dd8cdd65c8adc
Celeste Hayes <hayes.celeste93@gmail.com>
2024-11-08 22:14:05 -0500

    Markdown function updates

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,59 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'none') {
+    return '![GitHub License](https://img.shields.io/github/license/:user/:repo)';
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'none') {
+    return '\n* [License](#license)\n';
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'none') {
+    return `## License
+    
+    This project is licensed under the ${license} license.`;
+  }
+  return "";
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${renderLicenseSection(data.license)}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  If you have any questions, please contact me at ${data.email}.
+  You can also find me on GitHub at [${data.username}](
 `;
 }
 
